7c5128ee1e3a9da2de73ad9425bc4a353e788ac9
Scott Aaron <dr_saaron@yahoo.com>
2023-06-08 13:18:58 -0500

    more cleanup

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -6,21 +6,16 @@
 
 // A tic-tac-toe game, based on demo code at https://codepen.io/gaearon/pen/gWWZgR?editors=0100
 
-import React, {Component} from 'react';
 import Game from './Game';
 import { Provider } from 'react-redux';
 import store from '../store/TicTacToeStore';
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+const App = (props) => {
+    return (
+        <Provider store={store}>
+            <Game />
+        </Provider>
+    );
+};
 
-    render() {
-        return (
-                <Provider store={store}>
-                    <Game />
-                </Provider>
-                );
-    }
-}
+export default App;
