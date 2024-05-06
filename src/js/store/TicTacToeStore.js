7566b239e5493587120f59cf55d24ba8c7dc21a9
AAR1069 <scottaaron@northwesternmutual.com>
2024-05-06 13:15:58 -0500

    version update

--- src/js/store/TicTacToeStore.js
+++ src/js/store/TicTacToeStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/rootReducer';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import logger from 'redux-logger';
 
 const store = createStore(rootReducer, {}, applyMiddleware(thunk, logger));
